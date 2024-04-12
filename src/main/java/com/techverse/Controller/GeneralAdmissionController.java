72f67898b6519e7eb5498c4302c6b9002507a155
laxmivpatil <laxmi.patil@techverse.world>
2024-04-12 13:36:13 +0530

    change in api8

--- src/main/java/com/techverse/Controller/GeneralAdmissionController.java
+++ src/main/java/com/techverse/Controller/GeneralAdmissionController.java
@@ -72,153 +72,158 @@ public class GeneralAdmissionController {
 			@RequestPart(value = "profile", required = false) MultipartFile profile,
 			@RequestPart(value = "sssmid", required = false) MultipartFile sssmid) {
 
-		/*
-		 * if(type.equals("general")) { GeneralAdmission createdAdmission = new
-		 * GeneralAdmission(firstName,lastName, gender,dateOfBirth,
-		 * admissionClass,fatherName, motherName, mobileNo, email, type, "", "", "","",
-		 * "", "","", "", "","");
-		 * 
-		 * 
-		 * String subject = "New Admission Request";
-		 * 
-		 * String body = "<html><body>" +
-		 * "<p>Dear Pragya School Admissions Committee,</p>" +
-		 * "<p>We are pleased to inform you that a new admission request has been received for the academic year 2024.</p>"
-		 * + "<p>Details of the applicant are as follows:</p>" + "<ul>" +
-		 * "<li>Full Name: " + firstName + " " + lastName + "</li>" + "<li>Gender: " +
-		 * gender + "</li>" + "<li>Date of Birth: " + dateOfBirth + "</li>" +
-		 * "<li>Class Applying For: " + admissionClass + "</li>" + "<li>Father's Name: "
-		 * + fatherName + "</li>" + "<li>Mother's Name: " + motherName + "</li>" +
-		 * "<li>Mobile Number: " + mobileNo + "</li>" + "<li>Email Address: " + email +
-		 * "</li>" + "</ul>" +
-		 * "<p>Please review the details. If further information or clarification is required, please feel free to contact us.</p>"
-		 * + "<p>Thank you for considering this admission request.</p>" +
-		 * "<p>Best regards,<br/>" + firstName + " " + lastName + "<br/>" + mobileNo +
-		 * "</p>" + "</body></html>";
-		 * 
-		 * // emailService.sendEmail(schoolEmail, subject, body);
-		 * 
-		 * 
-		 * 
-		 * String userSubject = "Your Admission Enquiry"; String userBody =
-		 * "<html><body>" + "<p>Dear " + firstName + " " + lastName + ",</p>" +
-		 * "<p>Thank you for your admission enquiry. We have received your request and will get back to you shortly.</p>"
-		 * + "<p>Best regards,<br/>Pragya School Admissions Team</p>" +
-		 * "</body></html>";
-		 * 
-		 * // emailService.sendEmail(email, userSubject, userBody);
-		 * 
-		 * // emailService.sendSimpleEmail(email, "New Admission", firstName+lastName);
-		 * return new
-		 * ResponseEntity<>(generalAdmissionRepository.save(createdAdmission),
-		 * HttpStatus.OK); } else { String birthC="",lastR="",parentA="",studentA="",
-		 * sssmi="", bankD="", castC="",transferC="", profileP=""; //name String
-		 * birthCN="",lastRN="",parentAN="",studentAN="", sssmiN="", bankDN="",
-		 * castCN="",transferCN="", profilePN=""; //extension String
-		 * birthT="",lastRT="",parentAT="",studentAT="", sssmiT="", bankDT="",
-		 * castCT="",transferCT="", profilePT="";
-		 * 
-		 * 
-		 * if (birthCertificate != null && !birthCertificate.isEmpty()) { birthT =
-		 * getExtension(birthCertificate);
-		 * 
-		 * birthCN = "Birth_Certificate" + " " + Instant.now().toEpochMilli();
-		 * 
-		 * birthC = storageService.uploadFileOnAzure(birthCertificate,
-		 * birthCN+"."+birthT );
-		 * 
-		 * }
-		 * 
-		 * if (lastResult != null && !lastResult.isEmpty()) { lastRT=
-		 * getExtension(lastResult); lastRN= "Last_Year_Result" + " " +
-		 * Instant.now().toEpochMilli(); lastR =
-		 * storageService.uploadFileOnAzure(lastResult, lastRN+"."+lastRT); }
-		 * 
-		 * if (parentAadhar != null && !parentAadhar.isEmpty()) { parentAT=
-		 * getExtension(parentAadhar); parentAN = "Parent_Aadhar" + " " +
-		 * Instant.now().toEpochMilli(); parentA =
-		 * storageService.uploadFileOnAzure(parentAadhar,parentAN+"."+parentAT); }
-		 * 
-		 * if (studentAadhar != null && !studentAadhar.isEmpty()) { studentAT =
-		 * getExtension(studentAadhar); studentAN = "Student_Aadhar" + " " +
-		 * Instant.now().toEpochMilli(); studentA =
-		 * storageService.uploadFileOnAzure(studentAadhar, studentAN+"."+studentAT ); }
-		 * 
-		 * if (bankDoc != null && !bankDoc.isEmpty()) { bankDT = getExtension(bankDoc);
-		 * bankDN = "Bank_Doc" + " " + Instant.now().toEpochMilli() ; bankD =
-		 * storageService.uploadFileOnAzure(bankDoc, bankDN+"."+bankDT); }
-		 * 
-		 * if (cast != null && !cast.isEmpty()) { castCT = getExtension(cast); castCN =
-		 * "Cast" + " " + Instant.now().toEpochMilli(); castC =
-		 * storageService.uploadFileOnAzure(cast, castCN+"."+castCT); }
-		 * 
-		 * if (transferCertificate != null && !transferCertificate.isEmpty()) {
-		 * transferCT= getExtension(transferCertificate); transferCN =
-		 * "Transfer_Certificate" + " " + Instant.now().toEpochMilli(); transferC =
-		 * storageService.uploadFileOnAzure(transferCertificate,
-		 * transferCN+"."+transferCT); }
-		 * 
-		 * if (profile != null && !profile.isEmpty()) { profilePT =
-		 * getExtension(profile); profilePN = "Profile" + " " +
-		 * Instant.now().toEpochMilli() ; profileP =
-		 * storageService.uploadFileOnAzure(profile, profilePN+"."+profilePT); }
-		 * 
-		 * if (sssmid != null && !sssmid.isEmpty()) { sssmiT = getExtension(sssmid);
-		 * sssmiN = "SSSMID" + " " + Instant.now().toEpochMilli() ; sssmi =
-		 * storageService.uploadFileOnAzure(sssmid, sssmiN+"."+sssmiT); }
-		 * 
-		 * GeneralAdmission createdAdmission = new GeneralAdmission(firstName,lastName,
-		 * gender,dateOfBirth, admissionClass,fatherName, motherName, mobileNo, email,
-		 * type, pen, birthC, lastR,parentA, studentA, sssmi, bankD, castC, transferC,
-		 * profileP); String subject = "New Admission Request";
-		 * 
-		 * String body = "<html><body>" +
-		 * "<p>Dear Pragya School Admissions Committee,</p>" +
-		 * "<p>We are pleased to inform you that a new admission request has been received for the academic year 2024.</p>"
-		 * + "<p>Details of the applicant are as follows:</p>" + "<ul>" +
-		 * "<li>Full Name: " + firstName + " " + lastName + "</li>" + "<li>Gender: " +
-		 * gender + "</li>" + "<li>Date of Birth: " + dateOfBirth + "</li>" +
-		 * "<li>Class Applying For: " + admissionClass + "</li>" + "<li>Father's Name: "
-		 * + fatherName + "</li>" + "<li>Mother's Name: " + motherName + "</li>" +
-		 * "<li>Mobile Number: " + mobileNo + "</li>" + "<li>Email Address: " + email +
-		 * "</li>" + "<li>PEN: " + pen + "</li>" + "</ul>" +
-		 * "<p>Attached documents:</p>" + "<ul>" + "<li>Birth Certificate</li>" +
-		 * "<li>Previous Year's Result</li>" + "<li>Parent's Aadhar Card</li>" +
-		 * "<li>Student's Aadhar Card</li>" + "<li>Bank Documents</li>" +
-		 * "<li>Caste Certificate</li>" + "<li>Transfer Certificate</li>" +
-		 * "<li>Profile Picture</li>" + "</ul>" +
-		 * "<p>Please review the details and documents attached. If further information or clarification is required, please feel free to contact us.</p>"
-		 * + "<p>Thank you for considering this admission request.</p>" +
-		 * "<p>Best regards,<br/>" + firstName + " " + lastName + "<br/>" + mobileNo +
-		 * "</p>" + "</body></html>";
-		 * 
-		 * 
-		 * emailService.sendEmailWithAttachment(schoolEmail, subject, body,
-		 * birthCertificate, lastResult, parentAadhar, studentAadhar, bankDoc, cast,
-		 * transferCertificate, profile, sssmid);
-		 * 
-		 * String userSubject = "Your Admission Enquiry"; String userBody =
-		 * "<html><body>" + "<p>Dear " + firstName + " " + lastName + ",</p>" +
-		 * "<p>Thank you for your admission enquiry. We have received your request and will get back to you shortly.</p>"
-		 * + "<p>Best regards,<br/>Pragya School Admissions Team</p>" +
-		 * "</body></html>";
-		 * 
-		 * //emailService.sendEmail(email, userSubject, userBody);
-		 * 
-		 * 
-		 * 
-		 * return new
-		 * ResponseEntity<>(generalAdmissionRepository.save(createdAdmission),
-		 * HttpStatus.OK);
-		 * 
-		 * 
-		 * }
-		 */
 		
-		return new
-				  ResponseEntity<>(firstName,
+		  if(type.equals("general")) { GeneralAdmission createdAdmission = new
+		  GeneralAdmission(firstName,lastName, gender,dateOfBirth,
+		  admissionClass,fatherName, motherName, mobileNo, email, type, "", "", "","",
+		  "", "","", "", "","");
+		  
+		  
+		  String subject = "New Admission Request";
+		  
+		  String body = "<html><body>" +
+		  "<p>Dear Pragya School Admissions Committee,</p>" +
+		  "<p>We are pleased to inform you that a new admission request has been received for the academic year 2024.</p>"
+		  + "<p>Details of the applicant are as follows:</p>" + "<ul>" +
+		  "<li>Full Name: " + firstName + " " + lastName + "</li>" + "<li>Gender: " +
+		  gender + "</li>" + "<li>Date of Birth: " + dateOfBirth + "</li>" +
+		  "<li>Class Applying For: " + admissionClass + "</li>" + "<li>Father's Name: "
+		  + fatherName + "</li>" + "<li>Mother's Name: " + motherName + "</li>" +
+		  "<li>Mobile Number: " + mobileNo + "</li>" + "<li>Email Address: " + email +
+		  "</li>" + "</ul>" +
+		  "<p>Please review the details. If further information or clarification is required, please feel free to contact us.</p>"
+		  + "<p>Thank you for considering this admission request.</p>" +
+		  "<p>Best regards,<br/>" + firstName + " " + lastName + "<br/>" + mobileNo +
+		  "</p>" + "</body></html>";
+		  
+		  // emailService.sendEmail(schoolEmail, subject, body);
+		  
+		  
+		  
+		  String userSubject = "Your Admission Enquiry"; String userBody =
+		  "<html><body>" + "<p>Dear " + firstName + " " + lastName + ",</p>" +
+		  "<p>Thank you for your admission enquiry. We have received your request and will get back to you shortly.</p>"
+		  + "<p>Best regards,<br/>Pragya School Admissions Team</p>" +
+		  "</body></html>";
+		  
+		  // emailService.sendEmail(email, userSubject, userBody);
+		  
+		  // emailService.sendSimpleEmail(email, "New Admission", firstName+lastName);
+		 /* return new
+		  ResponseEntity<>(generalAdmissionRepository.save(createdAdmission),
+		  HttpStatus.OK);
+		  */
+		  return new
+				  ResponseEntity<>("Form Submitted successfully",
 				  HttpStatus.OK);
-
+		  } 
+		  else { 
+			  String birthC="",lastR="",parentA="",studentA="", sssmi="", bankD="", castC="",transferC="", profileP=""; //name String
+			  String birthCN="",lastRN="",parentAN="",studentAN="", sssmiN="", bankDN="",castCN="",transferCN="", profilePN=""; //extension String
+			  String birthT="",lastRT="",parentAT="",studentAT="", sssmiT="", bankDT="", castCT="",transferCT="", profilePT="";
+		  
+		  
+		  if (birthCertificate != null && !birthCertificate.isEmpty()) { birthT =
+		  getExtension(birthCertificate);
+		  
+		  birthCN = "Birth_Certificate" + " " + Instant.now().toEpochMilli();
+		  
+		  birthC = storageService.uploadFileOnAzure(birthCertificate,
+		  birthCN+"."+birthT );
+		  
+		  }
+		  
+		  if (lastResult != null && !lastResult.isEmpty()) { lastRT=
+		  getExtension(lastResult); lastRN= "Last_Year_Result" + " " +
+		  Instant.now().toEpochMilli(); lastR =
+		  storageService.uploadFileOnAzure(lastResult, lastRN+"."+lastRT); }
+		  
+		  if (parentAadhar != null && !parentAadhar.isEmpty()) { parentAT=
+		  getExtension(parentAadhar); parentAN = "Parent_Aadhar" + " " +
+		  Instant.now().toEpochMilli(); parentA =
+		  storageService.uploadFileOnAzure(parentAadhar,parentAN+"."+parentAT); }
+		  
+		  if (studentAadhar != null && !studentAadhar.isEmpty()) { studentAT =
+		  getExtension(studentAadhar); studentAN = "Student_Aadhar" + " " +
+		  Instant.now().toEpochMilli(); studentA =
+		  storageService.uploadFileOnAzure(studentAadhar, studentAN+"."+studentAT ); }
+		  
+		  if (bankDoc != null && !bankDoc.isEmpty()) { bankDT = getExtension(bankDoc);
+		  bankDN = "Bank_Doc" + " " + Instant.now().toEpochMilli() ; bankD =
+		  storageService.uploadFileOnAzure(bankDoc, bankDN+"."+bankDT); }
+		  
+		  if (cast != null && !cast.isEmpty()) { castCT = getExtension(cast); castCN =
+		  "Cast" + " " + Instant.now().toEpochMilli(); castC =
+		  storageService.uploadFileOnAzure(cast, castCN+"."+castCT); }
+		  
+		  if (transferCertificate != null && !transferCertificate.isEmpty()) {
+		  transferCT= getExtension(transferCertificate); transferCN =
+		  "Transfer_Certificate" + " " + Instant.now().toEpochMilli(); transferC =
+		  storageService.uploadFileOnAzure(transferCertificate,
+		  transferCN+"."+transferCT); }
+		  
+		  if (profile != null && !profile.isEmpty()) { profilePT =
+		  getExtension(profile); profilePN = "Profile" + " " +
+		  Instant.now().toEpochMilli() ; profileP =
+		  storageService.uploadFileOnAzure(profile, profilePN+"."+profilePT); }
+		  
+		  if (sssmid != null && !sssmid.isEmpty()) { sssmiT = getExtension(sssmid);
+		  sssmiN = "SSSMID" + " " + Instant.now().toEpochMilli() ; sssmi =
+		  storageService.uploadFileOnAzure(sssmid, sssmiN+"."+sssmiT); }
+		  
+		  GeneralAdmission createdAdmission = new GeneralAdmission(firstName,lastName,
+		  gender,dateOfBirth, admissionClass,fatherName, motherName, mobileNo, email,
+		  type, pen, birthC, lastR,parentA, studentA, sssmi, bankD, castC, transferC,
+		  profileP); String subject = "New Admission Request";
+		  
+		  String body = "<html><body>" +
+		  "<p>Dear Pragya School Admissions Committee,</p>" +
+		  "<p>We are pleased to inform you that a new admission request has been received for the academic year 2024.</p>"
+		  + "<p>Details of the applicant are as follows:</p>" + "<ul>" +
+		  "<li>Full Name: " + firstName + " " + lastName + "</li>" + "<li>Gender: " +
+		  gender + "</li>" + "<li>Date of Birth: " + dateOfBirth + "</li>" +
+		  "<li>Class Applying For: " + admissionClass + "</li>" + "<li>Father's Name: "
+		  + fatherName + "</li>" + "<li>Mother's Name: " + motherName + "</li>" +
+		  "<li>Mobile Number: " + mobileNo + "</li>" + "<li>Email Address: " + email +
+		  "</li>" + "<li>PEN: " + pen + "</li>" + "</ul>" +
+		  "<p>Attached documents:</p>" + "<ul>" + "<li>Birth Certificate</li>" +
+		  "<li>Previous Year's Result</li>" + "<li>Parent's Aadhar Card</li>" +
+		  "<li>Student's Aadhar Card</li>" + "<li>Bank Documents</li>" +
+		  "<li>Caste Certificate</li>" + "<li>Transfer Certificate</li>" +
+		  "<li>Profile Picture</li>" + "</ul>" +
+		  "<p>Please review the details and documents attached. If further information or clarification is required, please feel free to contact us.</p>"
+		  + "<p>Thank you for considering this admission request.</p>" +
+		  "<p>Best regards,<br/>" + firstName + " " + lastName + "<br/>" + mobileNo +
+		  "</p>" + "</body></html>";
+		  
+		  
+		  emailService.sendEmailWithAttachment(schoolEmail, subject, body,
+		  birthCertificate, lastResult, parentAadhar, studentAadhar, bankDoc, cast,
+		  transferCertificate, profile, sssmid);
+		  
+		  String userSubject = "Your Admission Enquiry"; String userBody =
+		  "<html><body>" + "<p>Dear " + firstName + " " + lastName + ",</p>" +
+		  "<p>Thank you for your admission enquiry. We have received your request and will get back to you shortly.</p>"
+		  + "<p>Best regards,<br/>Pragya School Admissions Team</p>" +
+		  "</body></html>";
+		  
+		  //emailService.sendEmail(email, userSubject, userBody);
+		  
+		  
+		  
+		 /* return new
+		  ResponseEntity<>(generalAdmissionRepository.save(createdAdmission),
+		  HttpStatus.OK);*/
+		  
+		  return new
+				  ResponseEntity<>("Form Submitted successfully",
+				  HttpStatus.OK);
+		  
+		  
+		  }
+		 
+		
+	
 	}
 
 	public String getExtension(MultipartFile file) {
