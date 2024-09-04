8e06158687c632f257c81d17c318cf59d7902e42
laxmivpatil <laxmi.patil@techverse.world>
2024-09-04 10:59:06 +0530

    This is second filetreyeytrt

--- src/main/java/com/techverse/Service/AchievementService.java
+++ src/main/java/com/techverse/Service/AchievementService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.techverse.Model.Achievement;
@@ -28,6 +29,7 @@ public class AchievementService {
     }
     
     
+    @Cacheable("achievementsByYear")
     public List<Achievement> getAchievementsByYear(String year) {
         return achievementRepository.findByYear(year);
     }
